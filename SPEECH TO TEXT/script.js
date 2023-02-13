6d2bfe5c4d9071db3fa5b1b617ea3c6cb1a9c000
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2023-02-13 12:09:29 +0300

    Final commit. Made all the neccessary functionalities of the site

---
+++ SPEECH TO TEXT/script.js
@@ -0,0 +1,72 @@
+let speechRecognition = window.webkitSpeechRecognition;
+let robot = new speechRecognition()
+robot.continuous = true;
+let startButton = document.querySelector('.start');
+let stopButton = document.querySelector('.stop')
+let guide = document.querySelector(".alert")
+
+let speech = ''
+
+startButton.addEventListener('click', (e) => {
+    if(speech.length){
+        speech += ' '
+    }
+    robot.start()
+    startButton.classList.add('hidden');
+    stopButton.classList.remove('hidden')
+})
+
+stopButton.addEventListener('click', (event) => {
+    stopButton.classList.add('hidden')
+    startButton.classList.remove('hidden')
+    robot.stop()  
+})
+
+robot.onstart = function(){
+    guide.classList.remove('hidden')
+    guide.textContent = 'Octopus robot is listening!'
+    setTimeout(() => {
+        guide.classList.add('hidden')
+        guide.textContent = ''
+    }, 2000)
+}
+
+robot.onspeechend = function(){
+    guide.classList.remove('hidden')
+    guide.textContent = 'Session ended!'
+    setTimeout(() => {
+        guide.classList.add('hidden')
+        guide.textContent = ''
+    }, 2000)
+    if(startButton.classList.contains('hidden')){
+        startButton.classList.remove('hidden')
+        stopButton.classList.add('hidden')
+    }
+}
+robot.onerror = function(){
+    guide.classList.remove('hidden')
+    guide.textContent = 'Error occured. Try again!'
+    setTimeout(() => {
+        guide.classList.add('hidden')
+        guide.textContent = ''
+    }, 2000)
+    if(startButton.classList.contains('hidden')){
+        startButton.classList.remove('hidden')
+        stopButton.classList.add('hidden')
+    }
+}
+
+robot.onresult = function(event){
+    let detected = event.resultIndex;
+    let transcript = event.results[detected][0].transcript;
+    speech += transcript;
+
+    guide.classList.remove('hidden')
+    guide.textContent = 'Octopus is generating text'
+    setTimeout(() => {
+        guide.classList.add('hidden')
+        guide.textContent = ''
+        document.querySelector('.transcript-injection').textContent = speech;
+    }, 1000)
+
+}
